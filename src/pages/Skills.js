b8194631a359e41e76eee69e8e538af8464072e0
jingyun-wong <jingyun.wong00@gmail.com>
2022-10-04 17:36:36 +0800

    Added responsive table for the skills for role

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,7 +1,78 @@
 import React from "react";
+import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Box from '@mui/material/Box';
 
-  
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: "#9CCAFF",
+        color: theme.palette.common.white
+
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+    borderLeft: 0,
+    borderRight:'0.5px solid grey',
+
+    '&:last-child': {
+        borderRight: 0,
+    },
+
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:last-child td, &:last-child th': {
+        borderBottom: 0,
+    },
+}));
+
+
+const rows = [
+    {'code':1,'name':'Risk Management','desc':'Risk Management is about the risk.'},
+    {'code':2,'name':'Risk Management','desc':'Risk Management is about the risk.'},
+    {'code':3,'name':'Risk Management','desc':'Risk Management is about the risk.'},
+    {'code':4,'name':'Risk Management','desc':'Risk Management is about the risk.'},
+    {'code':5,'name':'Risk Management','desc':'Risk Management is about the risk.'}
+]
+
 export default function Skills() {
-    return <h1>Skills</h1>;
+    return (
+        <Container sx={{mt:5}}>
+            <Box sx={{ typography: { xs: 'h6', md:'h4'}}}>Senior Manager</Box>
+            <Box sx={{ typography: { xs: 'body2', md:'h6'}}}>Skills Needed</Box>
 
+            <TableContainer component={Paper} sx={{marginTop:2}}>
+                <Table sx={{minWidth: 200}}>
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>Code No</StyledTableCell>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>Description</StyledTableCell>
+                            <StyledTableCell align='center'>Select Course</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <StyledTableRow key={row.name}>
+                                <StyledTableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>{row.code}</StyledTableCell>
+                                <StyledTableCell>{row.name}</StyledTableCell>
+                                <StyledTableCell sx={{ display: { xs: 'none', md: 'table-cell' } }}>{row.desc}</StyledTableCell>
+                                <StyledTableCell align='center'><ArrowForwardIosIcon></ArrowForwardIosIcon></StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
+    )
 }
