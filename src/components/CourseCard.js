5c77260a33d5f7856b6be785a0277732e88554a9
syafinazshahrinshan <89062054+syafinazshahrinshan@users.noreply.github.com>
2022-10-24 22:57:45 +0800

    link front and back end remove existing course

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
+
 import {Grid} from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -9,12 +11,27 @@ import Divider from '@mui/material/Divider';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
 function CourseCard(props){
+
+    function DeleteCourse(journeyID,courseID){
+        axios.delete('http://127.0.0.1:5000/journey/' + journeyID + "/" + courseID)
+            .then (() => {
+                window.location.reload(false)
+                console.log('Delete successful')
+
+            })
+            .catch(error => {
+                    console.log(error.message)
+            })
+    }      
+
     return (
         <Grid item xs={12} sm={6} md={3} sx={{display:'flex'}}>
             <Card variant="outlined" sx={{backgroundColor:"#f5f5f5"}} >
                 <CardContent sx={{px:2,py:1}}>
                     <Typography variant="h6" component="div">
+                        
                         {props.course}
+                        
                     </Typography>
                     <Divider/>
                     <Typography sx={{ mb: 1.5 }} color="text.secondary">
@@ -26,7 +43,7 @@ function CourseCard(props){
                 </CardContent>
                 { props.completed === false && props.status !== "Pending" && props.status !== "Retired" ?
                 <CardActions sx={{p:2, pt:0}}>
-                    <Button size="small" color="error">Remove Course 
+                    <Button size="small" color="error" onClick={DeleteCourse(props.journeyID,props.courseID)}>Remove Course 
                         <RemoveCircleOutlineIcon color="error" fontSize="small"></RemoveCircleOutlineIcon>
                     </Button>
                 </CardActions> : <></>}
