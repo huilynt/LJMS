338c75764720856e2c86f69652629bf80f80667c
Diyanah01 <diyanahjamal@gmail.com>
2022-10-29 13:06:34 +0800

    added main branch functions

--- api/app.py
+++ api/app.py
@@ -717,6 +717,100 @@ def get_courses_in_journey(journeyId):
         }
     ), 201
 
+@app.route("/courses/<string:courseId>")
+def get_assgined_courses(courseId):
+    skillList = Skill.query.all()
+    selected_skills = []
+
+    for skill in skillList:
+        courseList = skill.courses
+        course = Course.query.filter_by(Course_ID=courseId).first()
+        if course in courseList:
+            selected_skills.append(skill.Skill_ID)
+
+    if selected_skills:
+        return jsonify(
+            {
+                "code": 200,
+                "data": selected_skills,
+                "name": course.Course_Name
+            }
+        ), 201
+
+    return jsonify(
+        { 
+            "code": 404,
+            "message": "skills not found",
+            "name": course.Course_Name
+        }
+    ), 404
+
+#Add skills to existing course
+@app.route("/hr/courses/edit/<string:courseId>", methods=["POST"]) 
+def update_skills_to_course(courseId):
+    courseid = Course.query.filter_by(Course_ID = courseId).first().Course_ID
+    skillid_list = request.get_json() #get from the part when the specific skill is added
+    deleted_list=[]
+
+    if (db.session.query(Skill_course).filter_by(Course_ID=courseId).all()): #find all skills of courses and delete
+        existing_course_skills_list = db.session.query(Skill_course).filter_by(Course_ID=courseId).all()
+        for existing_course_skills in existing_course_skills_list:
+            deleted_list.append(existing_course_skills.Skill_ID)              
+            db.session.query(Skill_course).filter_by(Course_ID=courseId, Skill_ID=existing_course_skills.Skill_ID).delete()
+            db.session.commit()
+
+    if (skillid_list == []): #check if there is at least one skill selected
+        return jsonify(
+            {
+                "code": 400,
+                "message": "There must at least be one skill selected"
+            }
+        ), 400
+    
+    for skillid in skillid_list: #add skills to courses
+        course_skill = Skill_course.insert().values(Course_ID=courseId, Skill_ID=skillid)
+        db.engine.execute(course_skill)
+
+    return jsonify(
+        {
+            "code": 200,
+            "data": {
+                "Course": courseId,
+                "added list": skillid_list,
+                "deleted list": deleted_list
+            },
+            "message": "Course skills updated successfully."
+        }
+    ), 200
+
+@app.route("/journey/<string:journeyId>/<string:courseId>", methods=['DELETE'])
+def remove_existing_course_learning_journey(journeyId, courseId):   
+    learningjourney = LearningJourney.query.filter_by(Journey_ID=journeyId).first()
+    
+    for course in learningjourney.courses:
+        if(course.Course_ID==courseId):
+            
+            learningjourney.courses.remove(course)
+            db.session.commit()
+    
+            return jsonify(
+                        {
+                            "code": 200,
+                            "message": "Delete success"
+                        }
+                    ), 201
+
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "journeyId": journeyId,
+                "courseId": courseId
+            },
+            "message": "Course in selected Learning Journey not found"
+        }
+    ), 404
+
 # save learning journey with added courses
 @app.route("/journey/<string:staffId>/<string:jobRoleId>", methods=['POST'])
 def save_learning_journey(staffId, jobRoleId):
