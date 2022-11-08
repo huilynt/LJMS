62e2184959c2bac0b916a047f1acd4993bcbf588
Hui Lin <huilyn.thl@gmail.com>
2022-11-08 15:51:41 +0800

    unit test attempt

--- api/app.py
+++ api/app.py
@@ -2,11 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from Courses import *
-from Skills import *
-from JobRoles import *
-from Staffs import *
-from Journey import *
 
 app = Flask(__name__)
 app.config[
