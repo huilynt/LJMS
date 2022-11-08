1f865ed6e9ac150f05aa478cf13d3c65d4e7d333
syafinazshahrinshan <89062054+syafinazshahrinshan@users.noreply.github.com>
2022-11-08 21:32:16 +0800

    Update test_remove_existing_course_learning_journey.py

--- api/test/test_remove_existing_course_learning_journey.py
+++ api/test/test_remove_existing_course_learning_journey.py
@@ -25,63 +25,93 @@ class RemoveExistingCourse(TestApp):
 
     def test_existing_course_not_found(self):
         lj_obj = LearningJourney("EN002-140525","EN002",'140525','Progress')
-        
+        course1 = Course('FIN003',
+                            'Business Continuity Planning',
+                            'Business continuity planning is essential in any business to minimise loss when faced with potential threats and disruptions.',
+                            'Active','External','Finance')
+        course2 = Course('COR006',
+                            'Manage Change',
+                            'Identify risks associated with change and develop risk mitigation plans.',
+                            'Active','External','Core')
+        course3 = Course('COR002',
+                            'Lean Six Sigma Green Belt Certification',
+                            'Apply Lean Six Sigma methodology and statistical tools such as Minitab to be used in process analytics',
+                            'Active','Internal','Core')
+
         db.session.add(lj_obj)
+        db.session.add(course1)
+        db.session.add(course2)
+        db.session.add(course3)
+
+        lj_course1=LearningJourney_SelectedCourse.insert().values(Journey_ID='EN002-140525', Course_ID='COR002')
+        lj_course2=LearningJourney_SelectedCourse.insert().values(Journey_ID='EN002-140525', Course_ID='COR006')
+        db.engine.execute(lj_course1)
+        db.engine.execute(lj_course2)
+        
         db.session.commit()
-        response = self.client.delete("/journey/EN001-130001/COR001")
+        response = self.client.delete("/journey/EN002-140525/tch008")
         self.assertEqual(response.json,  {
             "code": 404,
             "data": {
                 "journeyId": 'EN002-140525',
-                "courseId": 'COR002'
+                "courseId": 'tch008'
             },
             "message": "Course in selected Learning Journey not found"
         })
-        self.assertEqual(response.status_code, 404)
+        
 
     def test_at_least_one_course(self):
         lj_obj = LearningJourney("EN001-130001","EN001",'130001','Progress')
-        lj_course = Course('COR001',
+        course = Course('COR001',
                             'Systems Thinking and Design',
                             'This foundation module aims to introduce students to the fundamental concepts and underlying principles of systems thinking',
                             'Active','Internal','Core')
+
         db.session.add(lj_obj)
-        db.session(lj_course)
-        LearningJourney_SelectedCourse.insert(Journey_ID='EN001-130001', Course_ID='COR001')
+        db.session.add(course)
+
+        lj_course=LearningJourney_SelectedCourse.insert().values(Journey_ID='EN001-130001', Course_ID='COR001')
+        
         db.session.commit()
+        db.engine.execute(lj_course)
 
         response = self.client.delete("/journey/EN001-130001/COR001")
         self.assertEqual(response.json, {
                                         "code": 200,
                                         "message": "Only one course left"
                                     })
-        self.assertEqual(response.status_code, 200)
+        
 
 
     def test_remove_existing_course(self):
         lj_obj = LearningJourney("EN002-140525","EN002",'140525','Progress')
-        lj_course1 = Course('COR002',
+        course1 = Course('COR002',
                             'Lean Six Sigma Green Belt Certification',
                             'Apply Lean Six Sigma methodology and statistical tools such as Minitab to be used in process analytics',
                             'Active','Internal','Core')
-        lj_course1 = Course('COR006',
+        course2 = Course('COR006',
                             'Manage Change',
                             'Identify risks associated with change and develop risk mitigation plans.',
-                            'Active','External','Core')
+                            'Active','External','Core')           
 
         db.session.add(lj_obj)
-        db.session.add(lj_course1)
-        db.session.add(lj_course2)
-        LearningJourney_SelectedCourse.insert(Journey_ID='EN002-140525', Course_ID='COR002')
-        LearningJourney_SelectedCourse.insert(Journey_ID='EN002-140525', Course_ID='COR006')
+        db.session.add(course1)
+        db.session.add(course2)
+
+        lj_course1=LearningJourney_SelectedCourse.insert().values(Journey_ID='EN002-140525', Course_ID='COR002')
+        lj_course2=LearningJourney_SelectedCourse.insert().values(Journey_ID='EN002-140525', Course_ID='COR006')
+        
         db.session.commit()
+
+        db.engine.execute(lj_course1)
+        db.engine.execute(lj_course2)
 
         response = self.client.delete("/journey/EN002-140525/COR002")
         self.assertEqual(response.json, {
                                         "code": 200,
                                         "message": "Delete success"
                                     })
-        self.assertEqual(response.status_code, 200)
+        
 
     
 
