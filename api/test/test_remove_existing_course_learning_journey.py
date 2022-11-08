258e504fcb027e5525e02e4802cde322c22863e9
syafinazshahrinshan <89062054+syafinazshahrinshan@users.noreply.github.com>
2022-11-08 17:12:58 +0800

    add test_at_least_once_course

--- api/test/test_remove_existing_course_learning_journey.py
+++ api/test/test_remove_existing_course_learning_journey.py
@@ -24,33 +24,59 @@ class TestApp(flask_testing.TestCase):
 class RemoveExistingCourse(TestApp):
 
     def test_existing_course_not_found(self):
-        lj_obj = LearningJourney("EN001-130001","EN001",'130001','Progress')
+        lj_obj = LearningJourney("EN002-140525","EN002",'140525','Progress')
         
-        db.session.add(lj_course)
+        db.session.add(lj_obj)
         db.session.commit()
         response = self.client.delete("/journey/EN001-130001/COR001")
         self.assertEqual(response.json,  {
             "code": 404,
             "data": {
-                "journeyId": 'EN001-130001',
-                "courseId": 'COR001'
+                "journeyId": 'EN002-140525',
+                "courseId": 'COR002'
             },
             "message": "Course in selected Learning Journey not found"
         })
         self.assertEqual(response.status_code, 404)
 
-    def test_remove_existing_course(self):
+    def test_at_least_one_course(self):
         lj_obj = LearningJourney("EN001-130001","EN001",'130001','Progress')
         lj_course = Course('COR001',
                             'Systems Thinking and Design',
-                            'This foundation module aims to introduce students to the fundamental concepts and underlying principles of systems thinking,',
+                            'This foundation module aims to introduce students to the fundamental concepts and underlying principles of systems thinking',
                             'Active','Internal','Core')
         db.session.add(lj_obj)
-        db.session.add(lj_course)
-        lj_courses = LearningJourney_SelectedCourse.insert(Journey_ID='EN001-130001', Course_ID='COR001')
+        db.session(lj_course)
+        LearningJourney_SelectedCourse.insert(Journey_ID='EN001-130001', Course_ID='COR001')
         db.session.commit()
 
         response = self.client.delete("/journey/EN001-130001/COR001")
+        self.assertEqual(response.json, {
+                                        "code": 200,
+                                        "message": "Only one course left"
+                                    })
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_remove_existing_course(self):
+        lj_obj = LearningJourney("EN002-140525","EN002",'140525','Progress')
+        lj_course1 = Course('COR002',
+                            'Lean Six Sigma Green Belt Certification',
+                            'Apply Lean Six Sigma methodology and statistical tools such as Minitab to be used in process analytics',
+                            'Active','Internal','Core')
+        lj_course1 = Course('COR006',
+                            'Manage Change',
+                            'Identify risks associated with change and develop risk mitigation plans.',
+                            'Active','External','Core')
+
+        db.session.add(lj_obj)
+        db.session.add(lj_course1)
+        db.session.add(lj_course2)
+        LearningJourney_SelectedCourse.insert(Journey_ID='EN002-140525', Course_ID='COR002')
+        LearningJourney_SelectedCourse.insert(Journey_ID='EN002-140525', Course_ID='COR006')
+        db.session.commit()
+
+        response = self.client.delete("/journey/EN002-140525/COR002")
         self.assertEqual(response.json, {
                                         "code": 200,
                                         "message": "Delete success"
