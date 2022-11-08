b9f8def003569c0c1165a5b156a266f1fa59f4b8
Hui Lin <huilyn.thl@gmail.com>
2022-11-08 17:08:24 +0800

    update requirements.txt. clean up workflow.yml"

--- api/test/test_skill.py
+++ api/test/test_skill.py
@@ -1,26 +1,7 @@
 import unittest
 
 from app import app
-from Skills import Skill
 import json
-
-
-class TestSkills(unittest.TestCase):
-    def test_skills_json(self):
-        skill = Skill(
-            "CM01",
-            "Change Management",
-            "For all approaches to prepare, support, and help organizations in making change.",
-        )
-        self.assertEqual(
-            skill.json(),
-            {
-                "Skill_ID": "CM01",
-                "Skill_Name": "Change Management",
-                "Skill_Desc": "For all approaches to prepare, support, and help organizations in making change.",
-                "Skill_Status": "",
-            },
-        )
 
 
 class AppTestCase(unittest.TestCase):
